a128832287e8e30b526222ddd1944deec74f2a15
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-04-24 17:46:04 -0400

    added bootstrap library

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -1,10 +1,14 @@
 import './Nutshell.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Nutshell() {
   return (
     <div className="Dashboard">
       <header className="App-header">
         <h1>Welcome to Nutshell</h1>
+        <Button>Test</Button>
       </header>
     </div>
   );
