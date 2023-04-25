0dc76533c2b018a7f2c184e459fd6c3f5c7eb117
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-04-25 12:56:50 -0400

    resolved issues, added logout functionality to nav

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -1,26 +1,34 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import "./NavBar.css"
-
-/* Very neat! bsPrefix overrides the default bootstrap class names. From the docs: 	
-Change the underlying component CSS base class name and modifier class names prefix. 
-This is an escape hatch for working with heavily customized bootstrap css. */
+import NavItem from 'react-bootstrap/NavItem';
+import { Link, useNavigate } from 'react-router-dom';
+import "./NavBar.css";
 
 const NavBar = () => {
+    const navigate = useNavigate();
+    const isLoggedIn = localStorage.getItem("nutshell_user");
+
     return (
-    <Navbar expand="lg" bg="navbar-background" variant="dark" fixed="top">
-        <Navbar.Brand href="#home" bsPrefix="navbar-brand-custom">Nutshell</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-            <Nav.Link href="#events" bsPrefix="nav-link-custom">Events</Nav.Link>
-            <Nav.Link href="#tasks" bsPrefix="nav-link-custom">Tasks</Nav.Link>
-            <Nav.Link href="#articles" bsPrefix="nav-link-custom">Articles</Nav.Link>
-            <Nav.Link href="#placeholder" bsPrefix="nav-link-custom">Logout</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-    )
+        <Navbar expand="lg" bg="navbar-background" variant="dark" fixed="top">
+            <Navbar.Brand href="#home" bsPrefix="navbar-brand-custom">Nutshell</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link href="#events" bsPrefix="nav-link-custom">Events</Nav.Link>
+                    <Nav.Link href="#tasks" bsPrefix="nav-link-custom">Tasks</Nav.Link>
+                    <Nav.Link href="#articles" bsPrefix="nav-link-custom">Articles</Nav.Link>
+                    {isLoggedIn &&
+                        <NavItem className="navbar__item navbar__logout">
+                            <Nav.Link bsPrefix="nav-link-custom" to="/" onClick={() => {
+                                localStorage.removeItem("nutshell_user");
+                                navigate("/", { replace: true });
+                            }}>Logout</Nav.Link>
+                        </NavItem>
+                    }
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
 
-export default NavBar
+export default NavBar;
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,5 +1,6 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import NavBar from "./NavBar"
+import { Outlet, Route, Routes } from "react-router-dom";
+import { Login } from "../auth/Login";
+import NavBar from "../nav/NavBar";
 
 export const ApplicationViews = () => {
   // Retrieving the user object from local storage
