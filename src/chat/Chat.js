d3ac3fbb231b9812288fb03ac93a75f416d31d4b
Kara Hall <karalynn.inc@gmail.com>
2023-04-27 09:22:19 -0400

    new main

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -2,10 +2,6 @@ import { Link } from "react-router-dom"
 
 export const Chat = ({ id, message, email }) => {
     return <section className="chat">
-        <div>
-            <Link to={`/users/${id}`}>UserName: {email}</Link>
-        </div>
-        <div>Message: {message}</div>
-        <div></div>
+       
     </section>
 }
